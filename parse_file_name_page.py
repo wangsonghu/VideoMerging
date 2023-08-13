f5d4c1fa3ec92845db3e08a6ae97b8a63fb1e3bc
gitea <gitea@nas0.hosts.songhu.wang>
2023-08-13 09:41:45 +0800

    up上传的合集python脚本

---
+++ parse_file_name_page.py
@@ -0,0 +1,27 @@
+import pathlib
+import json
+import sys
+import os
+
+file_path = sys.argv[1]
+# file_path = '/home/songhu/todo/VideoMerging/12046517/'
+path = pathlib.Path(file_path)
+file_list = path.glob('**/entry.json')
+ffmpeg_cmd_list= 'ffmpeg_cmd_list'
+if __name__ == '__main__':
+    with open(ffmpeg_cmd_list, 'a+') as file:
+        file.truncate(0)
+    for item in file_list:
+        with open(item, encoding='utf-8', mode='r') as f:
+            jdata = json.load(f)
+            title = jdata['title']
+            video_quality = str(jdata['video_quality'])
+            quality_pithy_description = jdata['quality_pithy_description']
+            page_index = jdata['page_data']['page']
+            page_index_title = jdata['page_data']['part']
+            page_bvid = jdata['bvid']
+            media_file_path = os.path.dirname(item)
+            audio_file = media_file_path+'/'+video_quality+'/'+'audio.m4s'
+            video_file = media_file_path+'/'+video_quality+'/'+'video.m4s'
+            with open(ffmpeg_cmd_list, encoding='utf-8', mode='a+') as fp:
+                print('sudo docker run -it --rm --name ffmpeg -v .:/config -w /config harbor.hosts.songhu.wang:8443/apps/linuxserver/ffmpeg:amd64-version-6.0-cli_v2023-08-09 -i %s -i %s -vcodec copy -acodec copy "[%s.][%s][%s·%s][%s].mkv"' % (audio_file, video_file, page_index, page_bvid, title, page_index_title, quality_pithy_description), file=fp)
